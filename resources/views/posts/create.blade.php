8493da298a08d749af7e4b6f09b48681940c9d03
wecandoitjapan <kazunori.fujimoto.1981@gmail.com>
2025-03-08 21:46:56 +0900

    10章 Bootstrapでデザインを整えよう

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,56 +5,78 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>新規投稿</title>
+   {{-- Bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    {{-- Google Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&display=swap" rel="stylesheet">
+
+    <!-- CSSの追加 -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" >
 </head>
 
 <body>
-   <header>
-       <nav>
-           <a href="{{ route('posts.index') }}">投稿アプリ</a>
+    <div class="wrapper">
+        <header>
+                <nav class="navbar navbar-light bg-light">
+                    <div class="container">
+                        <a href="{{ route('posts.index') }}" class="navbar-brand">投稿アプリ</a>
 
-           <ul>
-               <li>
-                   <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
-                   <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                       @csrf
-                   </form>
-               </li>
-           </ul>
-       </nav>
-   </header>
+                        <ul class="navbar-nav">
+                                <li class="nav-item">
+                                <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                    @csrf
+                                </form>
+                                </li>
+                        </ul>
+                    </div>
+            </nav>
+        </header>
 
-   <main>
-       <h1>新規投稿</h1>
+        <main>
+                <div class="container">
+                    <h1 class="fs-2 my-3">新規投稿</h1>
+                
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                    <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                    </ul>
+                </div>
+            @endif
+            <div class="mb-2">
+            <a href="{{ route('posts.index') }}" class="text-decoration-none">&lt; 戻る</a>
+                    </div>
 
-       @if ($errors->any())
-           <ul>
-               @foreach ($errors->all() as $error)
-                   <li>{{ $error }}</li>
-               @endforeach
-           </ul>
-       @endif
+                    <form action="{{ route('posts.store') }}" method="POST">
+                        @csrf
+                        <div class="form-group mb-3">
+                            <label for="title">タイトル</label>
+                            <!-- エラー時に入力内容が保持されるold()ヘルパ関数 -->
+                            <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
+                        </div>
+                        
+                        <div class="form-group mb-3">
+                            <label for="content">本文</label>
+                                <!-- エラー時に入力内容が保持されるold()ヘルパ関数 -->
+                                <textarea class="form-control" id="content" name="content">{{ old('content') }}</textarea>
+                        </div>
+                        <button type="submit" class="btn btn-outline-primary">投稿</button>
+                    </form>
+                </div>
+        </main>
 
-       <a href="{{ route('posts.index') }}">&lt; 戻る</a>
-
-       <form action="{{ route('posts.store') }}" method="POST">
-           @csrf
-           <div>
-               <label for="title">タイトル</label>
-               <!-- エラー時に入力内容が保持されるold()ヘルパ関数 -->
-               <input type="text" id="title" name="title" value="{{ old('title') }}">
-           </div>
-           <div>
-               <label for="content">本文</label>
-                <!-- エラー時に入力内容が保持されるold()ヘルパ関数 -->
-               <textarea id="content" name="content">{{ old('content') }}</textarea>
-           </div>
-           <button type="submit">投稿</button>
-       </form>
-   </main>
-
-   <footer>
-       <p>&copy; 投稿アプリ All rights reserved.</p>
-   </footer>
+        <footer class="d-flex justify-content-center align-items-center bg-light">
+            <p class="text-muted small mb-0">&copy; 投稿アプリ All rights reserved.</p>
+        </footer>
+    </div>
+    {{-- Bootstrap --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,65 +5,94 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>投稿一覧</title>
+   {{-- Bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    {{-- Google Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&display=swap" rel="stylesheet">
+
+    <!-- CSSの読み込み -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" >
 </head>
 
 <body>
-   <header>
-       <nav>
-           <a href="{{ route('posts.index') }}">投稿アプリ</a>
+    <div class="wrapper">
+            <header>
+                <nav class="navbar navbar-light bg-light">
+                    <div class="container">
+                        <a href="{{ route('posts.index') }}" class="navbar-brand">投稿アプリ</a>
 
-           <ul>
-               <li>
-                <!-- a要素にonclick属性を設定し、値にJavaScriptのイベント処理を指定 -->
-                   <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
-                   <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                       @csrf
-                   </form>
-               </li>
-           </ul>
-       </nav>
-   </header>
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <!-- a要素にonclick属性を設定し、値にJavaScriptのイベント処理を指定 -->
+                                <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                    @csrf
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+        </header>
 
    <main>
-       <h1>投稿一覧</h1>
-       <!-- フラッシュメッセージの表示エリア -->
-       @if (session('flash_message'))
-            <p>{{ session('flash_message') }}</p>
-        @endif
-    <!-- 編集ページでのエラーメッセージの表示エリア -->
-        @if (session('error_message'))
-            <p>{{ session('error_message') }}</p>
-        @endif
+        <div class="container">
+            <h1 class="fs-2 my-3">投稿一覧</h1>
+            
+            <!-- フラッシュメッセージの表示エリア -->
+            @if (session('flash_message'))
+                    <p class="text-success">{{ session('flash_message') }}</p>
+            @endif
+            
+            <!-- 編集ページでのエラーメッセージの表示エリア -->
+            @if (session('error_message'))
+                    <p class="text-danger">{{ session('error_message') }}</p>
+            @endif
 
-        <!-- 新規投稿ページへのリンク -->
-        <a href="{{ route('posts.create') }}">新規投稿</a>
-       @if($posts->isNotEmpty())
-           @foreach($posts as $post)
-               <article>
-                   <h2>{{ $post->title }}</h2>
-                   <p>{{ $post->content }}</p>
-                   <!-- 第2引数にモデルのインスタンスを渡している -->
-                   <a href="{{ route('posts.show', $post) }}">詳細</a>
-                   <!-- 投稿編集ページへのリンク -->
-                   <a href="{{ route('posts.edit', $post) }}">編集</a>
-                    
-                   <!-- 削除機能追加 -->
-                   <form action="{{ route('posts.destroy', $post) }}" method="POST" onsubmit="return confirm('本当に削除してもよろしいですか？');">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit">削除</button>
-                    </form>
+            <div class="mb-2">
+                <!-- 新規投稿ページへのリンク -->
+                <a href="{{ route('posts.create') }}" class="text-decoration-none">新規投稿</a>
+            </div>
+            
+            @if($posts->isNotEmpty())
+                @foreach($posts as $post)
+                                <article>
+                                    <div class="card mb-3">
+                                        <div class="card-body">
+                                                <h2 class="card-title fs-5">{{ $post->title }}</h2>
+                                                <p class="card-text">{{ $post->content }}</p>
+                                            
+                                                <div class="d-flex">  
+                                                    <!-- 第2引数にモデルのインスタンスを渡している -->
+                                                    <a href="{{ route('posts.show', $post) }}" class="btn btn-outline-primary d-block me-1">詳細</a>
+                                                    <!-- 投稿編集ページへのリンク -->
+                                                    <a href="{{ route('posts.edit', $post) }}" class="btn btn-outline-primary d-block me-1">編集</a>
+                                                    
+                                                    <!-- 削除機能追加 -->
+                                                    <form action="{{ route('posts.destroy', $post) }}" method="POST" onsubmit="return confirm('本当に削除してもよろしいですか？');">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="btn btn-outline-danger">削除</button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                </article>
+                    @endforeach
+                    @else
+                        <p>投稿はありません。</p>
+                    @endif
+        </div>
+    </main>
 
-               </article>
-           @endforeach
-       @else
-           <p>投稿はありません。</p>
-       @endif
-   </main>
-
-   <footer>
-       <p>&copy; 投稿アプリ All rights reserved.</p>
-   </footer>
+        <footer class="d-flex justify-content-center align-items-center bg-light">
+            <p>&copy; 投稿アプリ All rights reserved.</p>
+        </footer>
+    </div>
+   {{-- Bootstrap --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 
 </html>
